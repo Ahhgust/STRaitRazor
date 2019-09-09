25900fff70df1e2f5d0b5d9028495b5c05d94f95
awoerner <August.Woerner@unthsc.edu>
2019-09-09 16:39:48 -0500

    Added a simple script to compare labeled to unlabeled fastqs

---
+++ simpleCompare.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+if (@ARGV < 2) {
+    die "Gimme a labeled fastq file and the original!\n";
+}
+
+my %h;
+
+open IN, $ARGV[0] or
+    die "Doh!";
+
+while(<IN>){
+    chomp;
+    $h{$_}=1; # read the header
+    for (1..3) { # skip the rest
+        $_ = <IN>;
+    }
+}
+
+
+open IN, $ARGV[1] or
+    die "Doh!";
+
+my $printIt=0;
+
+while (<IN>) {
+    chomp;
+    if (!exists $h{$_}) {
+        print $_ , "\n";
+        for (1..3) {
+            $_ = <IN>;
+            print $_;
+        }
+    }
+
+}
+close IN;
+
+
